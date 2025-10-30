0a1b7b69b28582cc597561c169d7585cdc931f97
Elliot Nash <elliotnash@gmail.com>
2025-10-29 22:15:23 -0700

    feat(pio): Fallback to AP mode if network not available

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ AsyncWebServer server(80);
 extern const uint8_t index_html_start[] asm("_binary_dashboard_dist_index_html_gz_start");
 extern const uint8_t index_html_end[] asm("_binary_dashboard_dist_index_html_gz_end");
 
+IPAddress esp_ip;
+
 void setup()
 {
   // Initialize serial communication
@@ -25,13 +27,25 @@ void setup()
 
   // Connect to WiFi (WIFI_SSID/WIFI_PASSWORD are defined in .env)
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
+
+  // Attempt to connect to WiFi for 5 seconds
+  for (int attempts = 0; WiFi.status() != WL_CONNECTED && attempts < 5; attempts++) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
 
+  // If connection fails, run in access point mode
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connection failed, running in access point mode");
+    WiFi.disconnect();
+    delay(1000);
+    WiFi.softAP("ESP32 Distance Sensor", "password");
+    esp_ip = WiFi.softAPIP();
+  } else {
+    esp_ip = WiFi.localIP();
+  }
+
   Serial.println("Connected to WiFi");
-  Serial.println(WiFi.localIP());
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     AsyncWebServerResponse *response = request->beginResponse(200, "text/html", index_html_start, index_html_end - index_html_start);
@@ -40,6 +54,8 @@ void setup()
   });
 
   server.begin();
+
+  Serial.println("Dashboard running at: http://" + esp_ip.toString());
 }
 
 void loop()
@@ -55,5 +71,6 @@ void loop()
 
    // wait for a second
   delay(1000);
-  Serial.println(WiFi.localIP());
+
+  Serial.println("Dashboard running at: http://" + esp_ip.toString());
 }

b10901624891a4ff59036c0e3cae4ba1243b75d5
Elliot Nash <elliotnash@gmail.com>
2025-10-29 21:14:20 -0700

    fix(pio): Don't install already install python modules

--- scripts/install_deps.py
+++ scripts/install_deps.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python3
+import importlib.util
 Import("env")  # pyright: ignore[reportUndefinedVariable]
 
-env.Execute("$PYTHONEXE -m pip install dotenv")  # pyright: ignore[reportUndefinedVariable]
+def install_module(name):
+    if importlib.util.find_spec(name) is None:
+        env.Execute(f"$PYTHONEXE -m pip install {name}")  # pyright: ignore[reportUndefinedVariable]
+
+install_module("dotenv")
